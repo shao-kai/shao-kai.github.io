e9621b01be228db659420289143660bc2369e83a
shao-kai <snjdck@live.com>
2015-12-26 15:44:17 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,23 +1,44 @@
 (function(ext) {
+    var connectCode = 1;
+    function processInput(bytes){
+        connectCode = 2;
+        //console.log(String.fromCharCode.apply(null, bytes));
+    }
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+        return {status: connectCode, msg: 'Ready'};
+    };
+
+    ext._deviceConnected = function(dev) {
+        if (device) {
+            device.open({ bitRate: 115200, ctsFlowControl: 0 });
+            device.set_receive_handler(processInput);
+        }else{
+            tryNextDevice();
+        }
+    };
+    ext._deviceRemoved = function(dev) {
+        connectCode = 1;
     };
 
     // Functions for block with type 'w' will get a callback function as the 
     // final argument. This should be called to indicate that the block can
     // stop waiting.
     ext.wait_a = function(callback) {
+        var msg = new Uint8Array([0xff,0x55,0x08,0x00,0x02,0x08,0x07,0x02,0x00,0x00,0x14,0x00]);
+        device.send(msg.buffer);
         window.setTimeout(function() {
             callback(11);
         }, 1000);
     };
 
     ext.wait_b = function(callback) {
+        var msg = new Uint8Array([0xff,0x55,0x08,0x00,0x02,0x08,0x07,0x02,0x00,0x00,0x00,0x00]);
+        device.send(msg.buffer);
         window.setTimeout(function() {
             callback(23);
         }, 1000);
@@ -27,10 +48,10 @@
     var descriptor = {
         blocks: [
             ['R', 'wait a', 'wait_a'],
-             ['R', 'wait b', 'wait_b']
+            ['R', 'wait b', 'wait_b']
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Random wait extension', descriptor, ext);
+    ScratchExtensions.register('mCore', descriptor, ext, {type: 'serial'});
 })({});
